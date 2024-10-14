61ddefc18f7eea60d8bfef3a5c4391cf6524b74a
Amir Sarrafzadeh Arasi <sarrafzadeharasi.1871766@studenti.uniroma1.it>
2024-10-14 14:31:35 +0200

    main

--- app/main.py
+++ app/main.py
@@ -207,3 +207,6 @@ async def generate_cf(user: UserInput):
 
     return {"codiceFiscale": codice_fiscale}
 
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
